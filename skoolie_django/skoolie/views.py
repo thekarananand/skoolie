f52b6b5a7c56cc04d2d7c909e40fe8cae482ba5d
Karan Anand <thekarananand@gmail.com>
2023-09-01 23:23:24 +0530

    Login-Page Backend

--- skoolie_django/skoolie/views.py
+++ skoolie_django/skoolie/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def login(request):
+    return render(request, "skoolie/login.html")
+
+def dashboard(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        return HttpResponse(username + "   " + password)
+    
+    else:
+        return HttpResponse("not POST request")

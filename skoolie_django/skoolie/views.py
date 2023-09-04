208d4aef4244560da836d13a626026f697dd9840
Mamta Jakhar <dipijakhar01@gmail.com>
2023-09-04 16:51:45 +0530

    color setup for attendance

--- skoolie_django/skoolie/views.py
+++ skoolie_django/skoolie/views.py
@@ -31,14 +31,14 @@ def dashboard(request, StudentObject):
 
     if DETAINED == 1 :
         ATTENDANCE = f"Detained in 1 Subject"
-        ATTENDANCE_SHADE = "#FF0000"
+        ATTENDANCE_SHADE = "#FFC8C8"
     
     elif DETAINED > 1 :
         ATTENDANCE = f"Detained in {DETAINED} Subjects"
-        ATTENDANCE_SHADE = "#FF0000"
+        ATTENDANCE_SHADE = "#FFC8C8"
     else:
         ATTENDANCE = "All Good"
-        ATTENDANCE_SHADE = "#0000FF"
+        ATTENDANCE_SHADE = "#C8FFC8"
 
     return render(request, "skoolie/dashboard.html", {
         "GREET" : "Evening",
@@ -57,7 +57,11 @@ def attendance_display(rollno, subjects):
             n = n + 1
 
     return n
-
+def colouratt(percent):
+    if percent>=75:
+        return '#C8FFC8'
+    else:
+        return '#FFC8C8'
 def attendance(request):
     if request.method == "POST":
         rollno = request.POST.get('rollno', '')  # Use get() to avoid KeyError
@@ -74,15 +78,19 @@ def attendance(request):
                 percentage.append(PresentageAttendance(rollno, subject))
             except Exception as e:
                 print(f"Error processing attendance for {subject}: {e}")
+        for subs in ["CT","DST"]:
+            lst1=colouratt(PresentageAttendance(rollno, subject))
 
         return render(request, "skoolie/attendance.html", {
             "DATE" : "Mon, Sept 04",
             "CT_percentage": percentage[0],
             "CT_total": total[0],
             "CT_present": present[0],
+            "CT_color": lst1[0],
             "DST_percentage": percentage[1],
             "DST_total": total[1],
             "DST_present": present[1],
+            "DST_color": lst1[1],
         })
         
 
